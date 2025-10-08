27abd76abd62e028f0050c28559c7a0f28c1e82e
Xinwei702 <xinweiw963@gmail.com>
2025-10-08 11:39:32 +0800

    week05

---
+++ week05/week05-3.cpp
@@ -0,0 +1,12 @@
+///week05-3.cpp Part1 Part2
+#include <iostream>
+#include <sstream>
+#include <string>
+using namespace std;
+int main()
+{
+    string line;
+    while(getline(cin,line)){
+        cout<<line<<endl;
+    }
+}
