f07b75772357b276062702b7666571fa802a9f3e
13750093 <xinweiw963@gmaill.com>
2025-12-10 11:50:48 +0800

    week14

---
+++ week14/week14-2.cpp
@@ -0,0 +1,13 @@
+///week14-2.cpp
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head==nullptr || head->next==nullptr) return head;
+
+        ListNode* ans=reverseList(head->next);
+        head->next->next=head;
+        head->next=nullptr;
+        return ans;
+    }
+};
